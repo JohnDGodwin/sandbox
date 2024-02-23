bcef8c14e44e469ce80f2419c8aab4b7062b72f8
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-02-23 10:14:24 -0500

    Update record.sh

--- orange_pi5_plus/record.sh
+++ orange_pi5_plus/record.sh
@@ -14,7 +14,8 @@ if [ $RUNNING -eq 0 ]; then
         current_date=$(date +'%m-%d-%Y_%H-%M-%S')
 
         gst-launch-1.0 -e \
-        udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-nam>        rtph265depay ! \
+        udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H265' ! \
+        rtph265depay ! \
         h265parse ! \
         tee name=t ! \
         queue ! \
@@ -35,5 +36,3 @@ fi
 sleep 0.2
 fi
 sleep 0.1
-done
-
