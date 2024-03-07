ac452dc450a5aea9484904e5eda7e5a129a11ce1
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-03-06 21:57:36 -0500

    Update stream_nodvr.sh

--- radxa_pi_zero_3w/stream_nodvr.sh
+++ radxa_pi_zero_3w/stream_nodvr.sh
@@ -2,29 +2,10 @@
 
 export DISPLAY=:0
 
-RUNNING=0
-
-gpio_chip="gpiochip3"
-gpio_offset="8"
-
-while true; do
-    if [ $(sudo gpioget $gpio_chip $gpio_offset) -eq 1 ]; then
-    if [ $RUNNING -eq 0 ]; then
-
-        gst-launch-1.0 -e \
-            udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H265' ! \
-            rtph265depay ! \
-            h265parse ! \
-            mppvideodec ! \
-            videoconvert ! \
-            kmssink plane-id=76 &
-
-            RUNNING=$!
-    else
-            kill $RUNNING
-            RUNNING=0
-    fi
-    sleep 0.2
-    fi
-    sleep 0.1
-done
+gst-launch-1.0 -e \
+        udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H265' ! \
+        rtph265depay ! \
+        h265parse ! \
+        mppvideodec ! \
+        videoconvert ! \
+        rkximagesink plane-id=76
