6d611324d0cd16a5fa2a8abfd5b06d4308300f63
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-21 01:32:29 -0600

    Update commands.sh

--- tunnel/commands.sh
+++ tunnel/commands.sh
@@ -3,10 +3,17 @@
 FPS=60
 SIZE=1280x720
 
-# Double quotes for the outer string, escaped double quotes for the inner command
+update_fps() {
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 "sed -i \"/video0:/,/video1:/ s/fps: [0-9]*/fps: $FPS/\" /etc/majestic.yaml"
 echo "setting camera fps to $FPS"
+}
+
+update_size() {
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 "sed -i \"/video0:/,/video1:/ s/size: [0-9x]*/size: $SIZE/\" /etc/majestic.yaml"
 echo "setting camera resolution to $SIZE"
+}
+
+update_reboot(){
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'reboot'
 echo "rebooting camera..."
+}
