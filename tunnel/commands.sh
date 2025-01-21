384dae16ae663e67bdeeaa4607b5c05a50d46617
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-21 01:17:31 -0600

    Update commands.sh

--- tunnel/commands.sh
+++ tunnel/commands.sh
@@ -5,3 +5,4 @@ SIZE=1280x720
 
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/fps: [0-9]*/fps: $FPS/" /etc/majestic.yaml'
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/size: [0-9x]*/size: $SIZE/" /etc/majestic.yaml'
+sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'reboot'
