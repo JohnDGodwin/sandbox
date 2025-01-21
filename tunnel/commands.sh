bc326d59c88c22abfb448d47029e83aa93ce8cac
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-21 01:16:39 -0600

    Create commands.sh

---
+++ tunnel/commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FPS=60
+SIZE=1280x720
+
+sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/fps: [0-9]*/fps: $FPS/" /etc/majestic.yaml'
+sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/size: [0-9x]*/size: $SIZE/" /etc/majestic.yaml'
