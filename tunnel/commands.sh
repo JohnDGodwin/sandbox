1ed8218a47ec0a65e246abf7657ea070f02063f3
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-21 01:18:57 -0600

    Update commands.sh

--- tunnel/commands.sh
+++ tunnel/commands.sh
@@ -4,5 +4,8 @@ FPS=60
 SIZE=1280x720
 
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/fps: [0-9]*/fps: $FPS/" /etc/majestic.yaml'
+echo "setting camera fps to $FPS"
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'sed -i "/video0:/,/video1:/ s/size: [0-9x]*/size: $SIZE/" /etc/majestic.yaml'
+echo "setting camera resolution to $SIZE"
 sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 'reboot'
+echo "rebooting camera..."
